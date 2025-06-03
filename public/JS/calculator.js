391719d59ba2a32201769be007648c96fad646b9
Kevin Yugla <kiyugla@espe.edu.ec>
2025-06-03 08:53:32 -0500

    creacion de la funcion para agregar un numero a la pantalla y tambien de la funcion para seleccionar la operacion a realizar

--- public/JS/calculator.js
+++ public/JS/calculator.js
@@ -3,3 +3,31 @@ let numeroPrimero = '';
 let numeroSegundo = '';
 let operacionActual = null;
 let esSegundoNumero = false;
+// Referencia al campo de texto (pantalla de la calculadora)
+const pantalla = document.getElementById('display');
+// funcion para agregar un número a la pantalla
+function agregarNumero(num) {
+    if (num === ',' || num === '.') {
+        num = '.'; // unifica la coma y punto en punto decimal
+        const objetivo = esSegundoNumero ? numeroSegundo : numeroPrimero;
+        if (objetivo.includes('.')) return; // evitar que existan multiples puntos 
+    }
+
+    if (!esSegundoNumero) {
+        numeroPrimero += num;
+        pantalla.value = numeroPrimero;
+    } else {
+        numeroSegundo += num;
+        pantalla.value = numeroSegundo;
+    }
+}
+// funcoin para seleccionar la operacion
+function seleccionarOperacion(op) {
+    if (numeroPrimero === '') return;
+    operacionActual = op;
+    esSegundoNumero = true;
+    pantalla.value = ''; // limpia la pantalla y muestra operación brevemente
+    setTimeout(() => {
+        pantalla.placeholder = `Operación: ${op}`;
+    }, 100);
+}
